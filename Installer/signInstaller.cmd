955a86fbc85ce159877c6cdd445d064996ff60b3
Anna Krykora <krykoraanna@gmail.com>
2016-11-09 16:14:38 +0100

    Added more "Beta" signs. Added publisher and version info for Windows in installer. Added signing for CLIInterop.dll, Indexer++ Beta.exe, AddExplorerContextMenu.exe and separate script after nsi compilation to sign an installer self.

---
+++ Installer/signInstaller.cmd
@@ -0,0 +1,3 @@
+echo ========Signing Installer========
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" sign /t http://timestamp.verisign.com/scripts/timstamp.dll /f "anna_dev_cert.p12" /p o{P87=bD14Unj /d "Indexer++ Beta Installer.exe" "Indexer++ Beta Installer.exe"
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" verify /pa "Indexer++ Beta Installer.exe"
--- installerPickingUpScript.cmd
+++ installerPickingUpScript.cmd
@@ -1,14 +1,24 @@
 rem xcopy /s c:\source d:\target
-xcopy /y AddExplorerContextMenu\bin\Release\AddExplorerContextMenu.exe  Installer
-xcopy /y IndexerGUI\Icons\IndexerLogo.ico  			 						Installer
-xcopy /y IndexerGUI\Icons\icon_v3_2.ico  			 							Installer
-xcopy /y CLIInterop\Release\CLIInterop.dll								Installer
-xcopy /y IndexerGUI\bin\Release\Indexer++.exe			 				Installer
-xcopy /y IndexerGUI\bin\Release\UserSettings.xml			 			Installer
+copy /y AddExplorerContextMenu\bin\Release\AddExplorerContextMenu.exe   Installer
+copy /y IndexerGUI\Icons\IndexerLogo.ico  			 					Installer
+copy /y IndexerGUI\Icons\icon_v3_2.ico  			 					Installer
+copy /y CLIInterop\Release\CLIInterop.dll								Installer
+copy /y "IndexerGUI\bin\Release\Indexer++.exe"			 				"Installer\Indexer++ Beta.exe"
+copy /y IndexerGUI\bin\Release\UserSettings.xml			 				Installer
 
+rem sign "Indexer++ Beta.exe"
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" sign /t http://timestamp.verisign.com/scripts/timstamp.dll /f "anna_dev_cert.p12" /p o{P87=bD14Unj /d "Indexer++ Beta.exe" "Installer\Indexer++ Beta.exe"
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" verify /pa "Installer\Indexer++ Beta.exe"
+
+rem sign CLIInterop.dll
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" sign /t http://timestamp.verisign.com/scripts/timstamp.dll /f "anna_dev_cert.p12" /p o{P87=bD14Unj /d "CLIInterop.dll" "Installer\CLIInterop.dll"
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" verify /pa "Installer\CLIInterop.dll"
+
+rem sign AddExplorerContextMenu.exe
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" sign /t http://timestamp.verisign.com/scripts/timstamp.dll /f "anna_dev_cert.p12" /p o{P87=bD14Unj /d "AddExplorerContextMenu.exe" "Installer\AddExplorerContextMenu.exe"
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" verify /pa "Installer\AddExplorerContextMenu.exe"
+
+rem compile an installer. The Installer signing will be made after the compilation will be finished.
 makensis.exe Installer\IndexerInstaller.nsi
 
-"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" sign /t http://timestamp.verisign.com/scripts/timstamp.dll /f "anna_dev_cert.p12" /p Ol7*nF4GnpM#G /d "Indexer++ Beta Installer.exe" "Installer\Indexer++ Beta Installer.exe"
-
-"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" verify /pa "Installer\Indexer++ Beta Installer.exe"
 pause
