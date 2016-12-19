7c21863bfc27902fcc94f447781f88e2019d6a32
Ket <krykoraanna@gmai.com>
2016-12-19 18:14:11 +0100

    Fixed query serialization after adding "UseRegex" parameter.

--- Common/SearchQuery.cpp
+++ Common/SearchQuery.cpp
@@ -60,21 +60,22 @@ wstring SerializeQuery(const SearchQuery& query) {
 
     res += HelperCommon::U16stringToWstring(query.Text) + g_delim;           // 0
     res += HelperCommon::U16stringToWstring(query.SearchDirPath) + g_delim;  // 1
-    res += to_wstring(query.MatchCase) + g_delim;   // 2
+    res += to_wstring(query.MatchCase) + g_delim;                            // 2
+    res += to_wstring(query.UseRegex) + g_delim;                             // 3
 
-    res += to_wstring(query.SizeFrom) + g_delim;  // 3
-    res += to_wstring(query.SizeTo) + g_delim;    // 4
+    res += to_wstring(query.SizeFrom) + g_delim;  // 4
+    res += to_wstring(query.SizeTo) + g_delim;    // 5
 
-    res += to_wstring(query.ExcludeHiddenAndSystem) + g_delim;  // 5
-    res += to_wstring(query.ExcludeFolders) + g_delim;          // 6
-    res += to_wstring(query.ExcludeFiles) + g_delim;            // 7
+    res += to_wstring(query.ExcludeHiddenAndSystem) + g_delim;  // 6
+    res += to_wstring(query.ExcludeFolders) + g_delim;          // 7
+    res += to_wstring(query.ExcludeFiles) + g_delim;            // 8
 
-    res += to_wstring(query.CTimeFrom) + g_delim;  // 8
-    res += to_wstring(query.CTimeTo) + g_delim;    // 9
-    res += to_wstring(query.ATimeFrom) + g_delim;  // 10
-    res += to_wstring(query.ATimeTo) + g_delim;    // 11
-    res += to_wstring(query.MTimeFrom) + g_delim;  // 12
-    res += to_wstring(query.MTimeTo) + g_delim;    // 13
+    res += to_wstring(query.CTimeFrom) + g_delim;  // 9
+    res += to_wstring(query.CTimeTo) + g_delim;    // 10
+    res += to_wstring(query.ATimeFrom) + g_delim;  // 11
+    res += to_wstring(query.ATimeTo) + g_delim;    // 12
+    res += to_wstring(query.MTimeFrom) + g_delim;  // 13
+    res += to_wstring(query.MTimeTo) + g_delim;    // 14
 
     return move(res);
 }
@@ -82,25 +83,26 @@ wstring SerializeQuery(const SearchQuery& query) {
 uSearchQuery DeserializeQuery(const wstring& source) {
     auto parts = HelperCommon::Split(source, g_delim, HelperCommon::SplitOptions::INCLUDE_EMPTY);
 
-    auto text            = HelperCommon::WstringToU16string(parts[0]);
+    auto text = HelperCommon::WstringToU16string(parts[0]);
     auto search_dir_path = HelperCommon::WstringToU16string(parts[1]);
-    auto match_case      = HelperCommon::ParseNumber<bool>(parts[2]);
+    auto match_case = HelperCommon::ParseNumber<bool>(parts[2]);
+    auto use_regex = HelperCommon::ParseNumber<bool>(parts[3]);
 
-    auto size_from = HelperCommon::ParseNumber<int>(parts[3]);
-    auto size_to   = HelperCommon::ParseNumber<int>(parts[4]);
+    auto size_from = HelperCommon::ParseNumber<int>(parts[4]);
+    auto size_to = HelperCommon::ParseNumber<int>(parts[5]);
 
-    auto exclude_hidden_and_system = HelperCommon::ParseNumber<bool>(parts[5]);
-    auto exclude_folders           = HelperCommon::ParseNumber<bool>(parts[6]);
-    auto exclude_files             = HelperCommon::ParseNumber<bool>(parts[7]);
+    auto exclude_hidden_and_system = HelperCommon::ParseNumber<bool>(parts[6]);
+    auto exclude_folders = HelperCommon::ParseNumber<bool>(parts[7]);
+    auto exclude_files = HelperCommon::ParseNumber<bool>(parts[8]);
 
-    auto c_time_from = HelperCommon::ParseNumber<uint>(parts[8]);
-    auto c_time_to   = HelperCommon::ParseNumber<uint>(parts[9]);
-    auto a_time_from = HelperCommon::ParseNumber<uint>(parts[10]);
-    auto a_time_to   = HelperCommon::ParseNumber<uint>(parts[11]);
-    auto m_time_from = HelperCommon::ParseNumber<uint>(parts[12]);
-    auto m_time_to   = HelperCommon::ParseNumber<uint>(parts[13]);
+    auto c_time_from = HelperCommon::ParseNumber<uint>(parts[9]);
+    auto c_time_to = HelperCommon::ParseNumber<uint>(parts[10]);
+    auto a_time_from = HelperCommon::ParseNumber<uint>(parts[11]);
+    auto a_time_to = HelperCommon::ParseNumber<uint>(parts[12]);
+    auto m_time_from = HelperCommon::ParseNumber<uint>(parts[13]);
+    auto m_time_to = HelperCommon::ParseNumber<uint>(parts[14]);
 
-    auto query = make_unique<SearchQuery>(text, search_dir_path, match_case, size_from, size_to,
+    auto query = make_unique<SearchQuery>(text, search_dir_path, match_case, use_regex, size_from, size_to,
                                           exclude_hidden_and_system, exclude_folders, exclude_files, c_time_from,
                                           c_time_to, a_time_from, a_time_to, m_time_from, m_time_to);
 
