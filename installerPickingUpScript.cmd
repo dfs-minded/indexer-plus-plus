8d433387bbf7a46a331125b9197ff389f501f0eb
Anna Krykora <krykoraanna@gmail.com>
2018-03-27 18:06:13 +0200

    Updated installerPickingUpScript to use new signature procedure.

--- installerPickingUpScript.cmd
+++ installerPickingUpScript.cmd
@@ -12,42 +12,37 @@ xcopy /s/y wingup 														Installer\updater\
 
 set signToolPath="C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe"
 
-set timestampUrl=http://timestamp.verisign.com/scripts/timstamp.dll
-
-set pass=***
+set timestampUrl=http://time.certum.pl
+set owner="Open Source Developer, Anna Krykora"
 
 rem sign "Indexer++ Beta.exe"
-%signToolPath% sign /t %timestampUrl% /f "anna_dev_cert.p12" /p %pass% /d "Indexer++ Beta.exe" "Installer\Indexer++ Beta.exe"
+%signToolPath% sign /n %owner% /t %timestampUrl% /fd sha1 /v "Installer\Indexer++ Beta.exe"
 %signToolPath% verify /pa "Installer\Indexer++ Beta.exe"
 
-rem sign CLIInterop.dll
-%signToolPath%  sign /t %timestampUrl% /f "anna_dev_cert.p12" /p %pass% /d "CLIInterop.dll" "Installer\CLIInterop.dll"
+REM rem sign CLIInterop.dll
+%signToolPath%  sign /n %owner% /t %timestampUrl% /fd sha1 /v "Installer\CLIInterop.dll"
 %signToolPath%  verify /pa "Installer\CLIInterop.dll"
 
-rem sign AddExplorerContextMenu.exe
-%signToolPath%  sign /t %timestampUrl% /f "anna_dev_cert.p12" /p %pass% /d "AddExplorerContextMenu.exe" "Installer\AddExplorerContextMenu.exe"
+REM rem sign AddExplorerContextMenu.exe
+%signToolPath%  sign /n %owner% /t %timestampUrl% /fd sha1 /v "Installer\AddExplorerContextMenu.exe"
 %signToolPath%  verify /pa "Installer\AddExplorerContextMenu.exe"
 
-rem sign CloseRunningApp.exe
-%signToolPath%  sign /t %timestampUrl% /f "anna_dev_cert.p12" /p %pass% /d "CloseRunningApp.exe" "Installer\CloseRunningApp.exe"
+REM rem sign CloseRunningApp.exe
+%signToolPath%  sign /n %owner% /t %timestampUrl% /fd sha1 /v "Installer\CloseRunningApp.exe"
 %signToolPath%  verify /pa "Installer\CloseRunningApp.exe"
 
-rem sign AddDirToPathVarialble.exe
-%signToolPath%  sign /t %timestampUrl% /f "anna_dev_cert.p12" /p %pass% /d "AddDirToPathVarialble.exe" "Installer\AddDirToPathVarialble.exe"
+REM rem sign AddDirToPathVarialble.exe
+%signToolPath%  sign /n %owner% /t %timestampUrl% /fd sha1 /v "Installer\AddDirToPathVarialble.exe"
 %signToolPath%  verify /pa "Installer\AddDirToPathVarialble.exe"
 
-rem sign ifind.exe
-%signToolPath%  sign /t %timestampUrl% /f "anna_dev_cert.p12" /p %pass% /d "ifind.exe" "Installer\ifind.exe"
+REM rem sign ifind.exe
+%signToolPath%  sign /n %owner% /t %timestampUrl% /fd sha1 /v "Installer\ifind.exe"
 %signToolPath%  verify /pa "Installer\ifind.exe"
 
-rem compile an installer. The Installer signing will be made after the compilation will be finished.
+REM rem compile an installer. The Installer signing will be made after the compilation will be finished.
 makensis.exe Installer\IndexerInstaller.nsi
 
-echo Signing Installer after pressing any key. Be sure the installer file is ready to be signed.
-
-pause
-
-%signToolPath%  sign /t %timestampUrl% /f "anna_dev_cert.p12" /p %pass% /d "Indexer++ Beta Installer.exe" "Installer\Indexer++ Beta Installer.exe"
+%signToolPath%  sign /n %owner% /t %timestampUrl% /fd sha1 /v "Installer\Indexer++ Beta Installer.exe"
 %signToolPath%  verify /pa "Installer\Indexer++ Beta Installer.exe"
 
 pause
